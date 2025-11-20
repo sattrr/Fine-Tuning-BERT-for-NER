674906aaee6504a83bbabf6d62c19fdf13b4ca88
Lleviosa <satriokusum@gmail.com>
2025-11-20 11:41:05 +0700

    update gitignore

--- src/docling_models.py
+++
@@ -1,9 +0,0 @@
-from docling.utils import model_downloader
-import os
-
-target_dir = "C:/Users/Satriock/.cache/docling/models"
-
-os.makedirs(target_dir, exist_ok=True)
-model_downloader.download_models()
-
-print("Model downloaded to: " + target_dir)
